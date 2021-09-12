1f6e47b9b0dfe266d57494183e3c64fdcdd931fc
Mert Cobanov <mertcobanov@gmail.com>
2021-09-12 16:41:45 +0300

    hmac

---
+++ hmac.py
@@ -0,0 +1,12 @@
+import hashlib
+import hmac
+import binascii
+
+
+def hmac_sha256(key, msg):
+    return hmac.new(key, msg, hashlib.sha256).digest()
+
+
+key = b"12345"
+msg = b"sample message"
+print(binascii.hexlify(hmac_sha256(key, msg)))
